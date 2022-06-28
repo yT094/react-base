d371d4f51b3b73d5428345a57c1c408370ad33d4
ycs <1748780248@qq.com>
2022-06-28 13:24:12 +0800

    feat: add tabControl component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+/*
+ * @Author: ycs 1748780248@qq.com
+ * @Date: 2022-06-26 16:15:55
+ * @LastEditors: ycs 1748780248@qq.com
+ * @LastEditTime: 2022-06-28 13:22:39
+ * @FilePath: \react-base\src\index.js
+ * @Description: index 文件
+ */
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
-import "./style.scss";
+import App from "./04_组件通信案例/App";
+import "./04_组件通信案例/style.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
